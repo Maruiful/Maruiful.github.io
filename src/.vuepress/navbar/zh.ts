9c91cec23cf0039a4b803ffcaf6c5135244623e4
Maruiful <1599266196@qq.com>
2025-04-05 18:54:19 +0800

    中文导航栏修改测试4

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -3,6 +3,7 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/zh/",
   "/zh/demo/",
+  //下面是一个对象的示例
   // {
   //   text: "博文",
   //   icon: "pen-to-square",
@@ -48,41 +49,6 @@ export const zhNavbar = navbar([
     text: 'React系列',
     icon: 'react',
     link: '/react/'
-  },
-  {
-    text: 'Vue系列',
-    icon: 'vue',
-    link: '/vue/'
-  },
-  {
-    text: 'Vite系列',
-    icon: 'tool',
-    link: '/vite/'
-  },
-  {
-    text: '新框架尝鲜系列',
-    icon: 'geometry',
-    link: '/framework/'
-  },
-  {
-    text: '杂谈',
-    icon: 'article',
-    link: '/posts/'
-  },
-  {
-    text: '标签',
-    icon: 'tag',
-    link: '/tag/javascript/'
-  },
-  {
-    text: '分类',
-    icon: 'categoryselected',
-    link: '/category/前端/'
-  },
-  {
-    text: '时间轴',
-    icon: 'time',
-    link: '/timeline/'
   },
   {
     text: "V2 文档",
