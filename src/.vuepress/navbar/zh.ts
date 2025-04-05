c31d5ebb6549a87b263ef6de3f1ae5494f07c168
Maruiful <1599266196@qq.com>
2025-04-05 18:23:11 +0800

    中文导航栏修改测试

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -3,46 +3,86 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/zh/",
   "/zh/demo/",
+  // {
+  //   text: "博文",
+  //   icon: "pen-to-square",
+  //   prefix: "/zh/posts/",
+  //   children: [
+  //     {
+  //       text: "苹果",
+  //       icon: "pen-to-square",
+  //       prefix: "apple/",
+  //       children: [
+  //         { text: "苹果1", icon: "pen-to-square", link: "1" },
+  //         { text: "苹果2", icon: "pen-to-square", link: "2" },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     {
+  //       text: "香蕉",
+  //       icon: "pen-to-square",
+  //       prefix: "banana/",
+  //       children: [
+  //         {
+  //           text: "香蕉 1",
+  //           icon: "pen-to-square",
+  //           link: "1",
+  //         },
+  //         {
+  //           text: "香蕉 2",
+  //           icon: "pen-to-square",
+  //           link: "2",
+  //         },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+  //     { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+  //     "tomato",
+  //     "strawberry",
+  //   ],
+  // },
   {
-    text: "博文",
-    icon: "pen-to-square",
-    prefix: "/zh/posts/",
-    children: [
-      {
-        text: "苹果",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
-    ],
+    text: 'React系列',
+    icon: 'react',
+    link: '/react/'
+  },
+  {
+    text: 'Vue系列',
+    icon: 'vue',
+    link: '/vue/'
+  },
+  {
+    text: 'Vite系列',
+    icon: 'tool',
+    link: '/vite/'
+  },
+  {
+    text: '新框架尝鲜系列',
+    icon: 'geometry',
+    link: '/framework/'
+  },
+  {
+    text: '杂谈',
+    icon: 'article',
+    link: '/posts/'
+  },
+  {
+    text: '标签',
+    icon: 'tag',
+    link: '/tag/javascript/'
+  },
+  {
+    text: '分类',
+    icon: 'categoryselected',
+    link: '/category/前端/'
+  },
+  {
+    text: '时间轴',
+    icon: 'time',
+    link: '/timeline/'
   },
   {
     text: "V2 文档",
