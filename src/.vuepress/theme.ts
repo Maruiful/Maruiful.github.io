09a9ad79e0413fa57271cca02293d36b4d04de8e
Maruiful <1599266196@qq.com>
2025-09-13 23:34:33 +0800

    修改语言

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -59,24 +59,24 @@ export default hopeTheme({
 
   locales: {
     "/en/": {
-      // navbar
-      navbar: enNavbar,
+      // // navbar
+      // navbar: enNavbar,
 
-      // sidebar
-      sidebar: enSidebar,
+      // // sidebar
+      // sidebar: enSidebar,
 
-      footer: "Default footer",
+      // footer: "Default footer",
 
-      displayFooter: true,
+      // displayFooter: true,
 
-      blog: {
-        description: "A FrontEnd programmer",
-        intro: "/en/intro.html",
-      },
+      // blog: {
+      //   description: "A FrontEnd programmer",
+      //   intro: "/en/intro.html",
+      // },
 
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
+      // metaLocales: {
+      //   editLink: "Edit this page on GitHub",
+      // },
     },
 
     /**
